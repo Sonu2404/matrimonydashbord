a7418e2f9fea67a06a90045b0da5267efff299ab
sonu2404 <sonaliekhande12@gmail.com>
2025-03-31 17:51:34 +0530

    vites index.html

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // or 'modules' for wider browser support
+    chunkSizeWarningLimit: 1000, // Adjust as needed (in kBs)
+    sourcemap: true, // Enable sourcemaps for debugging
+  },
+  esbuild: {
+    logLevel: 'info', // or 'debug' for more detailed logs
+    minify: false, // Turn off minification for debugging
+  },
 });
