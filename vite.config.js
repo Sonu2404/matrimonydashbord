24a303f260ee22c2ced8139f20884e42a053eacf
sonu2404 <sonaliekhande12@gmail.com>
2025-03-31 14:31:08 +0530

    vites

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    minify: false, // Disable minification to avoid esbuild issues
-    sourcemap: true // Generate source maps for debugging
+    minify: false, // Disable minification
+    sourcemap: true
   }
 });
