3cdf0708daf638360b176f507560d411b0eca3c1
sonu2404 <sonaliekhande12@gmail.com>
2025-03-31 14:40:41 +0530

    vites

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Use this if you're facing issues with relative paths
-});
+export default {
+  build: {
+    rollupOptions: {
+      input: './src/main.js'
+    }
+  }
+}
