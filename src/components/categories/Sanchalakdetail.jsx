5f13578d210b6926a0b2696a3f3b8919b886a0ea
sonu2404 <sonaliekhande12@gmail.com>
2025-03-31 12:09:44 +0530

    sanchalak

--- src/components/categories/Sanchalakdetail.jsx
+++ src/components/categories/Sanchalakdetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa"; // Import social media icons
-import styles from "./sanchalakdetail.module.css";
+import styles from "./Sanchalakdetail.module.css";
 
 const MemberDetail = () => {
   const { id } = useParams();
