be375ec6b35e60fcd15ae2764e499dad1b08e940
sonu2404 <sonaliekhande12@gmail.com>
2025-03-31 14:38:02 +0530

    vites

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: false, // Disable minification
-    sourcemap: true
-  }
+  base: './', // Use this if you're facing issues with relative paths
 });
