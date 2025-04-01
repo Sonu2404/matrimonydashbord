4cfbb9b0301396e88dff0e41cb37200f198adac9
sonu2404 <sonaliekhande12@gmail.com>
2025-04-01 11:32:17 +0530

    vites

---
+++ index.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === "production") {
+    console.log = () => {};
+    console.error = () => {};
+    console.warn = () => {};
+  }
+  
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures assets load correctly
+  base: "/", // Ensure assets load correctly
+  server: {
+    port: 3000,
+    open: true, // Automatically open browser on local run
+  },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000,
+  },
 });
