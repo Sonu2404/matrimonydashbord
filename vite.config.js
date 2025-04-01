b73eafdec68f2e9ce9f932da91f90e0dde04c8ae
sonu2404 <sonaliekhande12@gmail.com>
2025-04-01 10:42:17 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  esbuild: {
-    target: 'esnext', // Ensure modern JavaScript support
-  },
+    plugins: [react()],
 });
